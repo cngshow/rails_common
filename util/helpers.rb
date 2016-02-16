b9839b2f5e36c9b1a151b1064d7a8d629bd53470
cshupp <cshupp@gmail.com>
2016-02-16 12:13:12 -0500

    Adding in code needed to link taxonomy tree to isaac rest.
    Reviewed by Greg Bowman and Cris Shupp

--- util/helpers.rb
+++ util/helpers.rb
@@ -1,13 +1,56 @@
+=begin
+Copyright Notice
+
+ This is a work of the U.S. Government and is not subject to copyright
+ protection in the United States. Foreign copyrights may apply.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=end
+require 'uri'
 module ETSUtilities
-    ##
-    # this method takes a camel cased word and changes it to snake case
-    # Example: EtsTooling -> ets_tooling
-    #
-    def to_snake_case(camel_cased_word)
-      camel_cased_word.to_s.gsub(/::/, '/').
-          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-          gsub(/([a-z\d])([A-Z])/,'\1_\2').
-          tr("-", "_").
-          downcase
+  ##
+  # this method takes a camel cased word and changes it to snake case
+  # Example: EtsTooling -> ets_tooling
+  #
+  def to_snake_case(camel_cased_word)
+    camel_cased_word.to_s.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+        gsub(/([a-z\d])([A-Z])/, '\1_\2').
+        tr("-", "_").
+        downcase
+  end
+
+  def json_to_yaml_file(json, file_name)
+    file_prefix = "./tmp/"
+    if Rails.env.development?
+      File.write("#{file_prefix}#{file_name}.yml",json.to_yaml)
+      $log.debug("Writing yaml file #{file_prefix}#{file_name}.yml.")
+    else
+      $log.debug("Not writing yaml file #{file_prefix}#{file_name}.yml. Rails.env = #{Rails.env}")
     end
+
+  end
+
+  def url_to_path_string(url)
+    begin
+      path = URI(url).path.gsub('/','_')
+      path = "no_path" if path.empty?
+      return path
+    rescue => ex
+      $log.error("An invalid url was given!")
+      $log.error(ex)
+    end
+    "bad_url"
+  end
+
 end
