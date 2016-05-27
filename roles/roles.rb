a0116713d0cedd9cfd31c556b2e18c1f3faf22b1
cshupp <cshupp@gmail.com>
2016-05-27 15:48:54 -0400

    Forgot this. This will break prisme.  Need to make prisme work with this than update rails common.

---
+++ roles/roles.rb
@@ -0,0 +1,27 @@
+module Roles
+
+  #good roles.
+  DEV_SUPER_USER = :dev_super_user
+  ADMINISTRATOR = :administrator
+
+  #loser roles.
+  READ_ONLY = :read_only
+  EDITOR = :editor
+  REVIEWER = :reviewer
+  APPROVER = :approver
+  FINAL_APPROVER = :final_approver
+
+  ALL_ROLES = [DEV_SUPER_USER, READ_ONLY, EDITOR, REVIEWER, APPROVER, FINAL_APPROVER, ADMINISTRATOR]
+
+  #session tags
+  SESSION_USER_ROLES = :current_user_roles
+  SESSION_USER = :current_user
+  SESSION_PASSWORD = :current_password
+  SESSION_LAST_ROLE_CHECK = :last_role_check
+
+  def self.valid_role?(role)
+    ALL_ROLES.include? role
+  end
+end
+
+
