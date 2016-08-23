a22cd7881ce32f47aaeca88376a862b9dd57102e
cshupp <cshupp@gmail.com>
2016-08-23 13:39:44 -0400

    updates to rails common ssoi code.

--- roles/ssoi.rb
+++ roles/ssoi.rb
@@ -3,15 +3,15 @@ require './lib/rails_common/roles/roles'
 module SSOI
 
   SSOI_USER = :ssoi_user_model
-  SSOI_USER_STRING = :ssoi_user
   SSOI_HEADER = :ssoi_header
   SSOI_ADSAMACCOUNTNAME = 'HTTP_ADSAMACCOUNTNAME'
 
   def ssoi_headers
     session[Roles::SESSION_ROLES_ROOT] ||= {}
     ssoi_user_name = request.headers[SSOI_ADSAMACCOUNTNAME]
-    return if ssoi_user_name.to_s.strip.empty?
-    session[Roles::SESSION_ROLES_ROOT][SSOI_USER_STRING] = ssoi_user_name
+    return nil if ssoi_user_name.to_s.strip.empty?
+    session[Roles::SESSION_ROLES_ROOT][Roles::SESSION_USER] = ssoi_user_name
+    session[Roles::SESSION_ROLES_ROOT][Roles::SESSION_PASSWORD] = nil
     $log.debug("The SSOI user is #{ssoi_user_name}")
     return ssoi_user_name
   end
