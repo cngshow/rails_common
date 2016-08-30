da4cbfbd044cbe25ae8099904df629250de92749
gpbowman-git <gpbowman.git@gmail.com>
2016-08-30 13:46:45 -0400

    modified read_ssoi_headers method call; fixed prisme_user call; modified ssoi_headers method

--- roles/ssoi.rb
+++ roles/ssoi.rb
@@ -10,8 +10,14 @@ module SSOI
   def ssoi_headers
     session[Roles::SESSION_ROLES_ROOT] ||= {}
     ssoi_user_name = request.headers[SSOI_ADSAMACCOUNTNAME]
+    $log.debug("I have header SSOI_ADSAMACCOUNTNAME? #{request.headers.key?(SSOI_ADSAMACCOUNTNAME)}")
     $log.debug("ssoi user is #{ssoi_user_name}")
-    return nil if ssoi_user_name.to_s.strip.empty?
+
+    if ssoi_user_name.to_s.strip.empty?
+      session[Roles::SESSION_ROLES_ROOT] = {} if request.headers.key?(SSOI_ADSAMACCOUNTNAME)
+      return nil
+    end
+
     session[Roles::SESSION_ROLES_ROOT][Roles::SESSION_USER] = ssoi_user_name
     session[Roles::SESSION_ROLES_ROOT][Roles::SESSION_PASSWORD] = nil
     $log.debug("The SSOI user is #{ssoi_user_name}")
