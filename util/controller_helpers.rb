4b8870c78903ce2bbc815405b2feea453d27ab8f
gpbowman-git <gpbowman.git@gmail.com>
2016-09-20 18:18:44 -0400

    refactored to remove deprecated method for setting up gon routes

--- util/controller_helpers.rb
+++ util/controller_helpers.rb
@@ -41,13 +41,13 @@ module CommonController
     def setup_routes
         original_verbosity = $VERBOSE
         $VERBOSE = nil
-        routes = Rails.application.routes.named_routes.helpers.to_a
+        routes = Rails.application.routes.named_routes.helper_names
         $VERBOSE = original_verbosity
         @@routes_hash ||= {}
-        if (@@routes_hash.empty?)
+        if @@routes_hash.empty?
             routes.each do |route|
                 begin
-                    @@routes_hash[route.to_s] = self.send(route)
+                    @@routes_hash[route] = self.send(route)
                 rescue ActionController::UrlGenerationError => ex
                     if (ex.message =~ /missing required keys: \[(.*?)\]/)
                         keys = $1
@@ -60,7 +60,7 @@ module CommonController
                         keys.each do |key|
                             required_keys_hash[key.to_sym] = ':' + key.to_s
                         end
-                        @@routes_hash[route.to_s] = self.send(route, required_keys_hash)
+                        @@routes_hash[route] = self.send(route, required_keys_hash)
                     else
                         raise ex
                     end
